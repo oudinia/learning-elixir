34aca8844a76b225b839296d49b2a6c2259f0d69
Oussama Dinia <oussama.dinia@gmail.com>
2024-06-20 15:07:16 +0200

    datetime

--- lib/example.ex
+++ lib/example.ex
@@ -12,6 +12,13 @@ defmodule Example do
       :world
 
   """
+  def time_feature do
+    time = Time.new!(16, 30, 0, 0)
+    date = Date.new!(2025, 1, 1)
+    date_time = DateTime.new!(date, time, "Etc/UTC")
+    IO.inspect(date_time)
+  end
+
   def main do
     IO.puts(Float.ceil(1.5))
     IO.puts(Integer.gcd(20, 10))
@@ -23,3 +30,4 @@ end
 
 
 Example.main()
+Example.time_feature()
